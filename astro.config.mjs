0677320d605e152a166b10dd1d9f92e3a36ebe7b
quietpulse <amanme9285@gmail.com>
2025-05-20 18:55:11 +0530

    feat: scaffold Astro portfolio
      Create the initial Astro and Tailwind portfolio with core layout, page sections,
      global styling, assets, package metadata, and project configuration.

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import path from 'path';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "static",
+  site: "https://token-template.deno.dev",
+  integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark-high-contrast",
+    },
+  },
+    vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
+});
---
+++ src/consts.ts
@@ -0,0 +1,26 @@
+export const SITE_TITLE = "Aqua";
+export const SITE_DESCRIPTION =
+  `A Site made by Aqua. It's my portfolio is astro and tailwind. Click and chack it out!`.trim();
+
+export const KNOWN_TECH =
+  `Astro,Tailwindcss,Javascript,Python,CSS,HTML,C,C++,Bash,VIM,React,Git,Photoshop,Figma,Obsidian`.split(
+    ",",
+  );
+export const ABOUT_ME =
+  `I'm a self-taught engineer and passionate technology enthusiast from India, currently a high school student. My interests span a wide range of fields including web development, Android development, graphic design, low-level programming, IoT, and more. I'm also a fan of anime and music, which inspire my creativity. While I'm still early in my journey, I'm highly motivated and committed to continuous learning and growth in the tech world.`.trim();
+export const GITHUB_USERNAME = "quietpulse";
+export const QUOTE = "Engineer, GFX designer and";
+export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+  {
+    title: "Telegram",
+    href: "//t.me/quietpulses"
+  },
+  {
+    title: "Github",
+    href: "//github.com/" + GITHUB_USERNAME,
+  },
+  {
+    title: "Pinterest",
+    href: "//pinterest.com/quietpulsess/"
+  },
+];
