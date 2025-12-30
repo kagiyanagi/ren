45006e0ee20e339365435cc134d61c065459dc33
kagiyanagi <amanme9285@gmail.com>
2025-12-30 13:34:03 +0530

    build(vercel): add generated deployment output
      Commit Vercel adapter output and dependency updates while adjusting deployment
      configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercelAdapter from "@astrojs/vercel/serverless";
 import path from "path";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
+  output: "server",
   site: "https://aquakun.vercel.app/",
+  adapter: vercelAdapter(),
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
